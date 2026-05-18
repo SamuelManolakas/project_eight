600880c84f900e3c92beca2a554aa7cb53087796
Samuel <samimanolakas@gmail.com>
2026-05-18 16:32:56 +0200

    Boss bullet update
    Fixed the shooting function of the boss so that it spawns a bullet for all the clients.
    Also changed some logic for the bullets direction

--- Assets/Boss/Scripts/Bullet_B.cs
+++ Assets/Boss/Scripts/Bullet_B.cs
@@ -1,7 +1,8 @@
 using System;
+using Unity.Netcode;
 using UnityEngine;
 
-public class Bullet_B : MonoBehaviour
+public class Bullet_B : NetworkBehaviour
 {
      public int damage;
      public float speed;
@@ -9,24 +10,41 @@ public class Bullet_B : MonoBehaviour
      public Vector3 direction;
      [HideInInspector]
      public Rigidbody _rigidbody;
+     
+     private Vector3 _direction;
 
      private void Start()
      {
           _rigidbody = GetComponent<Rigidbody>();
      }
+     
+     public void Initialize(Vector3 direction)
+     {
+          _direction = direction.normalized;
+     }
 
      private void Update()
      {
-          _rigidbody.linearVelocity = direction.normalized * speed;
+          if (!IsServer) return;
+
+          transform.position += _direction * speed * Time.deltaTime;
      }
 
      private void OnTriggerEnter(Collider other)
      {
+          if (!IsServer) return;
+          
           if (other.TryGetComponent(out HurtBox hurtBox))
           {
                hurtBox.GetHit(damage); 
           }
           
-          Destroy(this.gameObject, 0.2f);
+          DestroyBullet();
+     }
+     
+     private void DestroyBullet()
+     {
+          if (NetworkObject.IsSpawned)
+               NetworkObject.Despawn(true); // true = destroy the GameObject too
      }
 }
